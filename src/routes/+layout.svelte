e82db938d09f1f644c37c62834d83c75bf161fec
wintersalmon <wintersalmon@gmail.com>
2024-10-23 14:14:23 +0900

    Remove template files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,9 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>
+			visit <a href="https://github.com/wintersalmon/">wintersalmon | github</a> to learn more
+		</p>
 	</footer>
 </div>
 
